70329a45098a1810ee9a06d6fc3dedee9a78b4b4
Pivotass <maxime.di-natale@epitech.eu>
2021-01-15 01:57:29 +0100

    pourquoi

--- src/App.svelte
+++ src/App.svelte
@@ -1,3 +1,32 @@
-<main>
-	<h1>Hello :)</h1>
+<script>
+	import { slide, fade } from "svelte/transition"
+	import Page from "./Page.svelte";
+
+	let password = "";
+	let isCorrect = false;
+	let changePage = false;
+	let timeout
+
+	$: {
+		isCorrect = password === "Fighting gold"
+
+		if (isCorrect && !timeout && !changePage) {
+			timeout = setTimeout(() => {
+				changePage = true;
+				clearTimeout(timeout)
+			}, 3000)
+		}
+	}
+</script>
+
+{#if !changePage}
+<main transition:fade>
+	{#if isCorrect}
+		<h1 transition:slide>Bravo billy 😎</h1>
+	{:else}
+		<input transition:slide bind:value={password} type="password" placeholder="Un mot de passe ? 🧐">
+	{/if}
 </main>
+{:else}
+<Page />
+{/if}
---
+++ src/Page.svelte
@@ -0,0 +1,62 @@
+<script>
+    let date = "30 Février, 14h à la comédie"
+</script>
+
+<main>
+	<h1>Les choses que vous cherchez sont parfois sous vos yeux...</h1>
+    <h2>Même si elles ne sont pas forcément toujours visibles</h2>
+    
+    <p id="secret">{date}</p>
+</main>
+
+<style>
+	main {
+		padding: 0 30vw;
+		
+		animation: fadein 2000ms cubic-bezier(0.79,-0.01, 0.04, 1.06) forwards;
+	}
+
+	main > * {
+		cursor: default;
+		transition: 300ms cubic-bezier(0.79,-0.01, 0.04, 1.06) transform;
+	}
+
+	main > *:hover {
+		transform: scale(1.1);
+	}
+
+	main > *:not(:last-child) {
+		margin-bottom: 1em;
+    }
+    
+    #secret {
+        cursor: help;
+        position: fixed;
+
+        top: 10vh;
+        right: 30vw;
+        opacity: 0;
+
+        transition: 400ms cubic-bezier(0.79,-0.01, 0.04, 1.06);
+        transition-property: transform, opacity;
+    }
+
+    #secret:hover {
+        transform: scale(1.1);
+        opacity: 1;
+    }
+
+    #secret:active {
+        transform: scale(0.9);
+        opacity: .5;
+    }
+
+	@keyframes fadein {
+		0% {
+			transform: scale(1.2);
+			opacity: 0;
+		}
+		50% { opacity: 1 }
+		100% { transform: scale(1) }
+	}
+</style>
